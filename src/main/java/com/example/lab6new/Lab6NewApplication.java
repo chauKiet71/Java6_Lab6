efe7f549f15b344e9724aa231c1bafcd656df428
= <=>
2024-07-26 15:36:46 +0700

    done lab6

---
+++ src/main/java/com/example/lab6new/Lab6NewApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab6new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab6NewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab6NewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab6new/Lab6NewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab6new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab6NewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
